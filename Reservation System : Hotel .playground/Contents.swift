0d1f59e37bff4ef6637526f776e92cb021b3994f
AfafSaleh <Afaf.Saleh.med.s@tuwaiq.edu.sa>
2021-10-12 21:58:08 +0300

    First Project2

--- Reservation System : Hotel .playground/Contents.swift
+++ Reservation System : Hotel .playground/Contents.swift
@@ -1,5 +1,19 @@
 import UIKit
 
+//Can user log in
+let isLoggedIn:Bool = false
+if isLoggedIn {
+print("The system is logged in ✅ \n ----------------")
+} else {
+    print("The password you entered is incorrect ❌")
+}
+
+enum Roomcondition {
+  case available
+    case Unavailable
+}
+
+
 var hotellocation: Set<String>
 var RoomPrice: Dictionary<String,Int>
 hotellocation = []
@@ -7,64 +21,73 @@ hotellocation.insert("Jeedh")
 hotellocation.insert("Ryadh")
 hotellocation.insert("Dammam")
 
-let wel = "Welcom To Hotel\n----------------"
-print(wel)
 
-RoomPrice = [:]
-RoomPrice = ["Master Suite":600, "Mini-Suit":1115]
-//RoomPrice.insert("Master Suite" :600)
-//RoomPrice.insert("Mini-Suit")
-print("Hotel Location\n \(hotellocation)  \n ----------------")
-print("Room List and Price\n \(RoomPrice)  \n ----------------")
-
+print("Hotel Location 📍\n \(hotellocation)  \n ----------------")
 
 //Can user view type room
-class Hotel{
-    var Welcom: String = ""
-    func We(){
-        print("Room List and Price\n ") }
-}
-
-class Room:Hotel{
+//Can user view room list and prices
+struct Room {
     
-    var typeRoom: String
-    var roomNum: Int
-    var pricesroom: Double
+    var typeRoom: String = ""
+    var roomNum: Int = 0
+    var pricesroom: Double = 0
+    var options : [String] = []
+    var isAvailable: Roomcondition
     
-    override init(){
-        self.typeRoom = "Master Room"
-        self.roomNum = 302
-        self.pricesroom = 600.8
-    }
-    func available(Typrroom: String?){
-    
-        if Typrroom == nil {
-            print("✅ There are vacant rooms")
+    func checkIfRoomAvailable() -> String? {
+        if isAvailable == .available {
+            return "available"
         } else {
-            print("⭕️ No rooms")
+            return "Unavailable"
         }
     }
+}
+
+
+class Hotel {
+    var Welcom: String = ""
+    var rooms : [Room] = []
+    init(multipleRooms: [Room]){
+        self.rooms = multipleRooms
     }
 
-let we = Hotel()
-print("More details\n")
-let typ = Room()
-print("Type Room is : \(typ.typeRoom)")
-let num = Room()
-print("Room Number is : \(num.roomNum)")
-let PE = Room()
-print("Room Price is : \(PE.pricesroom)")
+    
+    func printAll() {
+        for we in rooms {
+            print("🔘Room List and Price💰\nTypes Room : \(we.typeRoom) \nRoom number : \(we.roomNum) \nRoom price : \(we.pricesroom)")
+        }
+    }
+}
 
-let ff = Room()
-ff.available(Typrroom: nil)
+let myHotel = Hotel(multipleRooms: [Room(typeRoom: "Master Room", roomNum: 101, pricesroom: 405.9, options: [], isAvailable: .Unavailable),
+                                  Room(typeRoom: "Mini-Suit", roomNum: 209, pricesroom: 600.8, options: [], isAvailable: .available)])
 
-//Can user view room list and prices
 
-var MasterRoom = Array<String>()
+myHotel.printAll()
+print("Room condition: ❌\(myHotel.rooms[0].isAvailable)")
+print("Room condition: ⭕️\(myHotel.rooms[1].isAvailable)")
+
+var MasterRoom = [String]()
 MasterRoom.append("Break Fast")
 MasterRoom.append("Swimming Pool")
 MasterRoom.append("WiFi")
 MasterRoom.append("Gym")
 
-// Newly Added
-MasterRoom.append("Gym-MoreWorkout")
+var PriceMR = [Int]()
+PriceMR.append(200)
+PriceMR.append(250)
+PriceMR.append(150)
+PriceMR.append(200)
+
+PriceMR.sort()
+print(" \(MasterRoom)\n \(PriceMR)")
+
+//Can user  reserve booking
+//Can user cancellation of rooms
+let bookingAndcancellation:Bool = false
+
+if bookingAndcancellation {
+    print(" --------------\n Room is booked")}
+else{
+    print(" --------------\n Room reservation has been cancelled")
+}
